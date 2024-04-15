11ae9cf28222c6050922f2c688d935e31ce9f042
bilal ahmet <129846356+bilal-ahmet@users.noreply.github.com>
2024-04-15 17:49:16 +0300

    Update constans.dart

--- lib/shared/constans.dart
+++ lib/shared/constans.dart
@@ -1,6 +1,6 @@
 class Constants{
-  static String AppId = "*";
-  static String apiKey = "*";
-  static String messagingSenderId = "*";
-  static String projectId = "*";
+  static String appId = "1:464997811328:web:779a3380003044f5ddd265";
+  static String apiKey = "AIzaSyDS9-PovCSGSTeg0HQ1r6jEGA_asfV-FfM";
+  static String messagingSenderId = "464997811328";
+  static String projectId = "flutter-my-chat-app-myself";
 }
