25fb6a22c27bef6e729619ceccc2c466e29ce3a7
bilal-ahmet <akpinarbilalahmet@gmail.com>
2024-05-01 15:05:13 +0300

    login_UI_and_validator_settings

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:chat_app/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -8,10 +11,94 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final formkey = GlobalKey<FormState>();
+  String email = "";
+  String password = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Login Page"),),
-    );
+        /* appBar: AppBar(
+          backgroundColor: Theme.of(context).primaryColor,
+        ), */
+        body: SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+        child: Form(
+            key: formkey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  "Groupie",
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "Login now to see what thet are talking",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
+                ),
+                Image.asset("assets/login.png"),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(
+                    labelText: "email",
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      email = value;
+                    });
+                  },
+                  validator: (value) {
+                    return RegExp(
+                                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                            .hasMatch(value!)
+                        ? null
+                        : "please enter a valid email";
+                  },
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: textInputDecoration.copyWith(
+                    labelText: "password",
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      password = value;
+                    });
+                  },
+                  validator: (value) {
+                    if (value!.length < 6) {
+                      return "password must be at least 6 characters";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                ElevatedButton(onPressed: () {
+                  login();
+                }, child: Text("enter"))
+              ],
+            )),
+      ),
+    ));
+    
+  }
+
+  login(){
+    if(formkey.currentState!.validate()){}
   }
 }
--- lib/shared/constans.dart
+++ lib/shared/constans.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/material.dart';
+
 class Constants{
   static String appId = "1:464997811328:web:779a3380003044f5ddd265";
   static String apiKey = "AIzaSyDS9-PovCSGSTeg0HQ1r6jEGA_asfV-FfM";
   static String messagingSenderId = "464997811328";
   static String projectId = "flutter-my-chat-app-myself";
+  final primaryColor = const Color(0xFFee7b64);
 }
