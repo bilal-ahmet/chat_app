b2fccf5c99d596d891f49aae5b1eb8f5918043f5
bilal-ahmet <akpinarbilalahmet@gmail.com>
2024-05-09 22:43:45 +0300

    authentication_services

---
+++ lib/service/database_service.dart
@@ -0,0 +1,14 @@
+class DataBaseService{
+  final String? uid;
+
+  DataBaseService({required this.uid});
+
+  // reference for our collection
+
+  // updating the user data
+
+  Future updateUserData (String fullName, String email) async{
+    
+  }
+
+}
