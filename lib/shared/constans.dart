cb4cdf3d5678691512d17dbabf74d647fd12fc30
bilal ahmet <129846356+bilal-ahmet@users.noreply.github.com>
2024-06-16 19:08:15 +0300

    Update constans.dart

--- lib/shared/constans.dart
+++ lib/shared/constans.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Constants{
-  // don't show api key for github
-  static String appId = "***";
-  static String apiKey = "***";
-  static String messagingSenderId = "***";
-  static String projectId = "***";
+  static String appId = "1:464997811328:web:779a3380003044f5ddd265";
+  static String apiKey = "AIzaSyDS9-PovCSGSTeg0HQ1r6jEGA_asfV-FfM";
+  static String messagingSenderId = "464997811328";
+  static String projectId = "flutter-my-chat-app-myself";
   final primaryColor = const Color(0xFFee7b64);
 }
